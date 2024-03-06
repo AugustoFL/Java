5a9444d4215ecac9cceb44bea1e2f29239e69571
Augusto Farias de Lima <113651842+AugustoFL@users.noreply.github.com>
2024-03-06 16:43:11 -0300

    Add files via upload

---
+++ Repetição/Ex1.java
@@ -0,0 +1,16 @@
+package repeticao;
+
+import java.util.Iterator;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		for (int j = 0; j < 50; j++) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
